77a0fd544ab5747847efe2e5e8df7c681b221ea4
dUlukbek <Dushaliev@gmail.com>
2019-02-04 20:14:12 +0600

    First commit1

---
+++ src/smth/Main.java
@@ -0,0 +1,14 @@
+package smth;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    }
+// track отследить, индексировать
+//git init - создание локального ГИТ репозитория
+// git status - печатает текущие изменения в проекте
+// git add . - добавить все изменения во всех файлах/ проиндексировать все изменения
+// git commit -
+// git config - - global user.email "Dushaliev@gmail.com"
